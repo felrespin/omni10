d70640c8b60d00f20257fdbada34ff8d8eadf12a
Felipe R Pinto <resendepinto@gmail.com>
2020-02-12 01:59:30 -0300

    teste comentario

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from './services/Api';
-
+//teste comentario git
 import './Global.css';
 import './App.css';
 import './Sidebar.css';
